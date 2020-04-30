54c53055345b4318f1c204a3fd88320443add93e
srieger <srieger256@gmail.com>
2020-04-30 19:34:44 -0400

    make all components needed

--- PS6/src/app/app.module.ts
+++ PS6/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputFormComponent } from './input-form/input-form.component';
+import { ResultDisplayComponent } from './result-display/result-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormComponent,
+    ResultDisplayComponent
   ],
   imports: [
     BrowserModule
